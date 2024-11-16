ad60dd4666cee2f58a7037cf3d040257950f736c
serejawell <sev2316@yandex.ru>
2024-11-16 13:15:14 +0300

    Homework 24.2

--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from django_filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
